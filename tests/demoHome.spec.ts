1aca0d17a649e67f8f8e140a1f49488eda7af852
ritesh-v2 <ritesh.yadav@v2solutions.com>
2025-03-15 20:09:06 +0530

    Adding the Project files

--- tests/demoHome.spec.ts
+++ tests/demoHome.spec.ts
@@ -5,27 +5,56 @@ test.describe('This block is used for the demo project', () => {
 
     test.beforeEach(async ({ page }) => {
 
-        await page.goto('https://www.icc-cricket.com/tournaments/champions-trophy-2025')
+        await page.goto('https://www.icc-cricket.com/')
         
     })
 
-    // test('Verify the URL of the page consist of Current campaign', async ({ page }) => {
+    test('Verify the URL of the page consist of Current campaign', async ({ page }) => {
 
-    //     const curCamp = await page.url();
+        const curCamp = await page.url();
 
-    //     // await expect(page).toHaveURL('tournaments/champions-trophy-2025')
-
-    //     await expect(curCamp).toContain('tournaments/champions-trophy-2025');
-    
-    // })
-
-    test('Verify the title for home page  ', async ({ page }) => {
-
-        const titleHome = await page.getByTitle('ICC Champions Trophy, 2025');
-
-        await expect(titleHome).toContainText('ICC Champions Trophy, 2025');
+        await expect(curCamp).toContain('icc-cricket');
     
     })
 
+    test('Verify the title for home page',async ({ page }) => {
+
+        const titleFirst = await page.title();
+        await expect(titleFirst).toContain('Official International Cricket Council Website');
     
+    })
+
+    test('Verify the logo of the website ', async ({ page }) => {
+
+       await expect(page.getByAltText('Header Logo')).toBeVisible();
+        
+    })
+
+    test('Verify the search for term, its URL and Result for search', async ({ page }) => {
+
+        const searchButton = await page.locator('//a[@href="/search"]');
+
+        await searchButton.click();
+
+        await expect(page.url()).toContain('https://www.icc-cricket.com/search');
+
+        const textTerm = await page.getByPlaceholder('what are you looking for?');
+
+        await textTerm.fill('India');
+
+        await textTerm.press('Enter');
+
+        await expect(page.url).toContain('https://www.icc-cricket.com/search?q=india');
+
+    })
+
+    // test('Verify the menu links in the header section of home page', async ({ page }) => {
+
+    //     const menuLink = await page.locator('#primaryNavigation__i9x0aUyv9c_CsVXO5FBi a')
+
+    //         const text = await links.length()
+
+    //         console.log(text)
+    //     })
+        
 })
---
+++ tests/demoSearch.spec.ts
@@ -0,0 +1,31 @@
+import {test,expect} from "@playwright/test"
+import exp from "constants"
+
+test.describe('This block is used for the demo project', () => {
+
+    test.beforeEach(async ({ page }) => {
+
+        await page.goto('https://www.icc-cricket.com/')
+        
+    })
+
+    test('Verify the search for term, its URL and Result for search', async ({ page }) => {
+
+        const searchButton = await page.locator('//a[@href="/search"]');
+
+        await searchButton.click();
+
+        expect(page.url()).toContain('https://www.icc-cricket.com/search');
+
+        const textTerm = await page.getByPlaceholder('what are you looking for?');
+
+        await textTerm.fill('India');
+
+        await textTerm.press('Enter');
+
+        const serachURL = await page.url().toLowerCase()
+
+        expect(serachURL).toContain("https://www.icc-cricket.com/search?q=india")
+
+    })     
+})
