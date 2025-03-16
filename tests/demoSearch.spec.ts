d1b6522d8757a4fbdd05bfb1dfef93b374fe74de
ritesh-v2 <ritesh.yadav@v2solutions.com>
2025-03-16 21:23:57 +0530

    Fix the changes in firefox

--- tests/demoSearch.spec.ts
+++ tests/demoSearch.spec.ts
@@ -25,11 +25,11 @@ test.describe('This block is used for the demo project', () => {
 
         await textTerm.press('Enter');
 
-        await page.waitForTimeout(2000);
+        await page.waitForTimeout(5000);
 
         const serachURL = await page.url().toLowerCase()
 
-        expect(serachURL).toContain("https://www.icc-cricket.com/search?q=india")
+        await expect(serachURL).toContain("https://www.icc-cricket.com/search?q=india")
         
     })     
 })
